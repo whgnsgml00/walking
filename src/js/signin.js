3280879033509cb647717d9280270db14bc9841a
Richard <whgnsgml00@naver.com>
2022-08-17 16:12:26 +0900

    8/17 commit

--- src/js/signin.js
+++ src/js/signin.js
@@ -3,7 +3,7 @@ let password = document.getElementById('floatingPassword');
 const btnLogin = document.querySelector('.btn-primary');
 //이메일 비밀번호 일치한다면 로그인 버튼 눌렀을 때 로그인 완료 띄우고 index.html로 이동
 function moveIndex() {
-    if (email && password) {
+    if (email.value && password.value) {
         alert('로그인되었습니다.'); 
     } else {
         alert('아이디나 비밀번호를 확인하새요.');
